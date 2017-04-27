f2b6573ab37140a5d603c8edc8bcd3de116f2279
iqans <iqan.shaikh@gmail.com>
2017-04-27 10:54:13 +0100

    worker role added - should work

---
+++ Test-azure-service-bus/TestWorkerRole/CustomMessage.cs
@@ -0,0 +1,10 @@
+﻿
+namespace TestWorkerRole
+{
+    public class CustomMessage
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string Message { get; set; }
+    }
+}

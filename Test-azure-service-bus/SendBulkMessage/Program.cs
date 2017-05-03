c28abdff5b3b4c4c7338db430025874575c100fc
iqans <iqan.shaikh@gmail.com>
2017-05-03 09:11:50 +0100

    added send bulk message program

---
+++ Test-azure-service-bus/SendBulkMessage/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+
+namespace SendBulkMessage
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var connectionString = "Endpoint=sb://iqanstest1.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=kACru3WDbVWYmD5GEFF81sLIsgi+eyR9fjeO6+NWYpY=";
+            var queueName = "MessagesQueue";
+            Console.WriteLine("Enter count: ");
+            var count = Console.ReadLine();
+            try
+            {
+                int c = int.Parse(count);
+                var client = QueueClient.CreateFromConnectionString(connectionString, queueName);
+                for (int i = 0; i < c; i++)
+                {
+                    client.Send(new BrokeredMessage(string.Format("Hi there! [Message:{0}]", c)));   
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+            Console.WriteLine("Done!");
+            Console.ReadLine();   
+        }
+    }
+}
